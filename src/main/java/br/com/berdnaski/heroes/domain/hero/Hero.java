21dcec40c467b16803744fab5b97c99aca97fa2c
berdnaski <berdnaski1177@gmail.com>
2025-11-27 15:41:45 -0300

    refactor: remove validation from Hero entity to follow layer separation

--- src/main/java/br/com/berdnaski/heroes/domain/hero/Hero.java
+++ src/main/java/br/com/berdnaski/heroes/domain/hero/Hero.java
@@ -1,9 +1,6 @@
 package br.com.berdnaski.heroes.domain.hero;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,22 +15,15 @@ public class Hero {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-    @NotBlank(message = "Nome não pode ser nulo ou vazio")
     @Column(nullable = false)
     private String name;
 
-    @NotNull(message = "Poder de ataque não pode ser nulo")
-    @Min(value = 1, message = "Poder de ataque deve ser maior do que zero")
     @Column(name = "attack_power", nullable = false)
     private Integer attackPower;
 
-    @NotNull(message = "Poder de defesa não pode ser nulo")
-    @Min(value = 1, message = "Poder de defesa deve ser maior do que zero")
     @Column(name = "defense_power", nullable = false)
     private Integer defensePower;
 
-    @NotNull(message = "Vida não pode ser nulo")
-    @Min(value = 1, message = "Vida deve ser maior do que zero")
     @Column(nullable = false)
     private Integer health;
 }
