0962c0b2e761a5eb22524e9486d8dcace42fa302
berdnaski <berdnaski1177@gmail.com>
2025-11-27 15:20:26 -0300

    chore: initial project setup with spring boot and postgresql

---
+++ src/main/java/br/com/berdnaski/heroes/HeroesBattleApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.berdnaski.heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesBattleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesBattleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/berdnaski/heroes/HeroesBattleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.berdnaski.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesBattleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
