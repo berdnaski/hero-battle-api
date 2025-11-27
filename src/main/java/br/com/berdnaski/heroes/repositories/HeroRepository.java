dcab7f95bc5e4bc3272fe561f73aebf1a808757a
berdnaski <berdnaski1177@gmail.com>
2025-11-27 16:08:38 -0300

    feat: create Hero repository with JPA and specifications support

--- src/main/java/br/com/berdnaski/heroes/repositories/HeroRepository.java
+++ src/main/java/br/com/berdnaski/heroes/repositories/HeroRepository.java
@@ -1,4 +1,14 @@
 package br.com.berdnaski.heroes.repositories;
 
-public interface HeroRepository {
+import br.com.berdnaski.heroes.domain.hero.Hero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface HeroRepository extends JpaRepository<Hero, UUID>, JpaSpecificationExecutor<Hero> {
+    Optional<Hero> findByName(String name);
 }
