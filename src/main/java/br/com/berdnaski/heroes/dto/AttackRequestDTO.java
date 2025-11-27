24d0097cf424d559673411f452888daa4a327ef2
berdnaski <berdnaski1177@gmail.com>
2025-11-27 15:45:29 -0300

    feat: add battle DTOs for attack functionality

--- src/main/java/br/com/berdnaski/heroes/dto/AttackRequestDTO.java
+++ src/main/java/br/com/berdnaski/heroes/dto/AttackRequestDTO.java
@@ -1,4 +1,16 @@
 package br.com.berdnaski.heroes.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AttackRequestDTO {
+    @NotNull(message = "Ataque não pode ser nulo ou vazio")
+    @Min(value = 1, message = "Ataque deve ser maior do que zero")
+    private Integer attackValue;
 }
--- src/main/java/br/com/berdnaski/heroes/dto/MessageResponseDTO.java
+++ src/main/java/br/com/berdnaski/heroes/dto/MessageResponseDTO.java
@@ -1,4 +1,12 @@
 package br.com.berdnaski.heroes.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageResponseDTO {
+    private String message;
 }
